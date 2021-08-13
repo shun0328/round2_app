f857ed12585ad4030f5dfdeee5e16e9922798d17
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-08-13 20:02:12 +0900

    画面遷移の基盤の作成

---
+++ lib/event_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EventView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: double.infinity,
+          color: Colors.amberAccent,
+        ),
+      )
+
+    );
+  }
+
+}
---
+++ lib/profile_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Container(
+            height: double.infinity,
+            color: Colors.red,
+          ),
+        )
+
+    );
+  }
+
+}
