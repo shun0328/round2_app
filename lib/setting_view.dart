209e5bc4fcc863a8c6ebcddb09d6f841353585b8
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-08-13 20:30:30 +0900

    画面遷移の基盤の作成

---
+++ lib/setting_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SettingView extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Container(
+            height: double.infinity,
+            color: Colors.blue,
+          ),
+        )
+
+    );
+  }
+
+}
