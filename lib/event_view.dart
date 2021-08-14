38a81336a3484842820b21f2f34015d916a339bf
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-08-14 13:54:42 +0900

    プロフィール画面の実装

--- lib/event_view.dart
+++ lib/event_view.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class EventView extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    // 画面のサイズを取得
+    final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
         backgroundColor: Colors.black54,
 
@@ -12,34 +15,33 @@ class EventView extends StatelessWidget{
           backgroundColor: Colors.white,
         ),
 
-        body: Center(
-
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-
-              SizedBox(
-                width: 300,
-                height: 50,
-                child:ElevatedButton(
-                  child: const Text('トーナメント'),
-                  style: ElevatedButton.styleFrom(
-                    textStyle: TextStyle(fontSize: 20),
-                    primary: Colors.white,
-                    onPrimary: Colors.black,
+        body: Stack(
+            children: <Widget>[
+              // トーナメントボタンの配置
+              Positioned(
+                  left: size.width * 0.1,
+                  top: size.height * 0.1,
+                  width: size.width * 0.8,
+                  height: size.width * 0.15,
+                  child: ElevatedButton(
+                    child: const Text('トーナメント'),
+                    style: ElevatedButton.styleFrom(
+                      textStyle: TextStyle(fontSize: 20),
+                      primary: Colors.white,
+                      onPrimary: Colors.black,
+                    ),
+                    onPressed: () {},
                   ),
-                  onPressed: () {},
-                ),
               ),
 
-              SizedBox(
-                height: 20,
-              ),
 
-              SizedBox(
-                width: 300,
-                height: 50,
-                child:ElevatedButton(
+              // リングゲームボタンの配置
+              Positioned(
+                left: size.width * 0.1,
+                top: size.height * 0.22,
+                width: size.width * 0.8,
+                height: size.width * 0.15,
+                child: ElevatedButton(
                   child: const Text('リングゲームの開催状況'),
                   style: ElevatedButton.styleFrom(
                     textStyle: TextStyle(fontSize: 20),
@@ -50,95 +52,96 @@ class EventView extends StatelessWidget{
                 ),
               ),
 
-              SizedBox(
-                height: 70,
-              ),
 
-              Row(
-                children: [
-                  
-                  Expanded(
-                      child: Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          //color: Colors.black54,
-                          borderRadius: BorderRadius.circular(8.0),
-                          image: DecorationImage(
-                            image: AssetImage(
-                              'images/instagram.png',
-                            ),
-                            //fit: BoxFit.cover,
-                          ),
-                        ),
-                        child: Material(
-                          color: Colors.transparent,
-                          child: InkWell(
-                            borderRadius: BorderRadius.circular(8.0),
-                            onTap: () {
-                              // 押下時の処理を記述
-                            },
-                          ),
-                        ),
+              // インスタグラムボタンの配置
+              Positioned(
+                left: size.width * 0.1,
+                top: size.height * 0.4,
+                width: size.width * 0.2,
+                height: size.width * 0.2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'images/instagram.png',
                       ),
-                  ),
-
-
-                  Expanded(
-                      child: Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          //color: Colors.lightBlueAccent,
-                          borderRadius: BorderRadius.circular(8.0),
-                          image: DecorationImage(
-                            image: AssetImage(
-                              'images/twitter.png',
-                            ),
-                            //fit: BoxFit.cover,
-                          ),
-                        ),
-                        child: Material(
-                          color: Colors.transparent,
-                          child: InkWell(
-                            borderRadius: BorderRadius.circular(8.0),
-                            onTap: () {
-                              // 押下時の処理を記述
-                            },
-                          ),
-                        ),
-                      ),
-                  ),
-
-
-                  Expanded(
-                    child: Container(
-                      width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                        //color: Colors.lightBlueAccent,
-                        borderRadius: BorderRadius.circular(8.0),
-                        image: DecorationImage(
-                          image: AssetImage(
-                            'images/line.png',
-                          ),
-                        ),
-                      ),
-                      child: Material(
-                        color: Colors.transparent,
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(8.0),
-                          onTap: () {
-                            // 押下時の処理を記述
-                          },
-                        ),
-                      ),
+                      //fit: BoxFit.cover,
                     ),
                   ),
-                ],
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(8.0),
+                      onTap: () {
+                        // 押下時の処理を記述
+                      },
+                    ),
+                  ),
+                ),
+              ),
+
+
+              // ツイッターボタンの配置
+              Positioned(
+                top: size.height * 0.4,
+                left: size.width * 0.5 - size.width * 0.1,
+                width: size.width * 0.2,
+                height: size.width * 0.2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'images/twitter.png',
+                      ),
+                      //fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(8.0),
+                      onTap: () {
+                        // 押下時の処理を記述
+                      },
+                    ),
+                  ),
+                ),
+              ),
+
+
+              // ラインボタンの配置
+              Positioned(
+                top: size.height * 0.4,
+                right: size.width * 0.1,
+                width: size.width * 0.2,
+                height: size.width * 0.2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'images/line.png',
+                      ),
+                      //fit: BoxFit.cover,
+                    ),
+                  ),
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(8.0),
+                      onTap: () {
+                        // 押下時の処理を記述
+                      },
+                    ),
+                  ),
+                ),
               )
-            ],
-          ),
+
+
+
+            ]
         )
     );
   }
--- lib/profile_view.dart
+++ lib/profile_view.dart
@@ -3,6 +3,26 @@ import 'package:flutter/material.dart';
 class ProfileView extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    // 画面のサイズを取得
+    final Size size = MediaQuery.of(context).size;
+    // カードの中の文字の色
+    final Color topColor = Colors.white;
+    // カード以外の文字の色
+    final Color bottomColor = Colors.white;
+
+    // ID
+    final String id = 'asdfghjkl';
+    // 名前
+    final String name = 'REONA';
+    // レート
+    final int rating = 6000;
+    // 来店回数
+    final int visit_time = 6;
+    // ポイント
+    final point = 270;
+    // チップ
+    final chip = 15000;
+
     return Scaffold(
         backgroundColor: Colors.black54,
 
@@ -12,70 +32,205 @@ class ProfileView extends StatelessWidget{
           backgroundColor: Colors.white,
         ),
 
-        body: Center(
-            child: Column(
-                children: [
+        body: Stack(
+            children: <Widget>[
 
-                  // 調整
-                  SizedBox(height: 50),
+              // カードの配置
+              Positioned(
+                  left: size.width * 0.1,
+                  top: size.height * 0.05,
+                  width: size.width * 0.8,
+                  child: Image.asset('images/bronze.png')
+              ),
 
-                  Stack(
-                      children: <Widget>[
-                        SizedBox(
-                            width: 360,
-                            child: Image.asset('images/bronze.png')
-                        ),
+              // IDの配置（カード内）
+              Positioned(
+                  left: size.width * 0.15,
+                  top: size.height * 0.065,
+                  child: Text(
+                    'ID：'+ id,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: topColor,
+                    ),
+                  )
+              ),
 
-                        Column(
-                          children: [
-                            // 調整
-                            SizedBox(height: 20),
+              // 名前の配置（カード内）
+              Positioned(
+                  left: size.width * 0.15,
+                  top: size.height * 0.12,
+                  child: Text(name,
+                    style: TextStyle(
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                      color: topColor,
+                    ),
+                  )
+              ),
 
-                            Text(
-                              '　ID：'+'asdfghjkl',
-                              style: TextStyle(
-                                //fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.black,
-                              ),
-                            ),
+              // レートの配置（カード内）
+              Positioned(
+                  left: size.width * 0.15,
+                  top: size.height * 0.21,
+                  child: Text(
+                    rating.toString()+ ' rt',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: topColor,
+                    ),
+                  )
+              ),
 
-                            // 高さ調整
-                            SizedBox(height: 10),
-
-                            Text(
-                              'Kaito',
-                              style: TextStyle(
-                                //fontWeight: FontWeight.bold,
-                                fontSize: 40,
-                                color: Colors.black,
-                              ),
-                            ),
-
-                            // 高さ調整
-                            SizedBox(height: 30),
-
-                            Text(
-                              '6000'+' rt',
-                              style: TextStyle(
-                                //fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.black,
-                              ),
-                            ),
-
-                          ],
-                        ),
-                      ]
+              // プロフィール画像の配置（カード内）
+              Positioned(
+                right: size.width * 0.15,
+                top: size.height * 0.07,
+                child: Container(
+                  width: 80.0,
+                  height: 80.0,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage("images/profile.png")
+                      )
                   ),
+                ),
+              ),
 
+              // IDの配置
+              Positioned(
+                  left: size.width * 0.1,
+                  top: size.height * 0.36,
+                  child: Text(
+                    'ID',
+                    style: TextStyle(
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 27,
+                      color: bottomColor,
+                    ),
+                  )
+              ),
+              Positioned(
+                  left: size.width * 0.47,
+                  top: size.height * 0.36,
+                  child: Text(
+                    id,
+                    style: TextStyle(
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 27,
+                      color: bottomColor,
+                    ),
+                  )
+              ),
 
+              // 名前の配置
+              Positioned(
+                  left: size.width * 0.1,
+                  top: size.height * 0.42,
+                  child: Text(
+                    '名前',
+                    style: TextStyle(
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 27,
+                      color: bottomColor,
+                    ),
+                  )
+              ),
+              Positioned(
+                  left: size.width * 0.47,
+                  top: size.height * 0.42,
+                  child: Text(
+                    name,
+                    style: TextStyle(
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 27,
+                      color: bottomColor,
+                    ),
+                  )
+              ),
 
+              // 来店回数の配置
+              Positioned(
+                  left: size.width * 0.1,
+                  top: size.height * 0.48,
+                  child: Text(
+                    '来店回数',
+                    style: TextStyle(
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 27,
+                      color: bottomColor,
+                    ),
+                  )
+              ),
+              Positioned(
+                  left: size.width * 0.47,
+                  top: size.height * 0.48,
+                  child: Text(
+                    visit_time.toString() +' 回',
+                    style: TextStyle(
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 27,
+                      color: bottomColor,
+                    ),
+                  )
+              ),
 
+              // ポイントの配置
+              Positioned(
+                  left: size.width * 0.1,
+                  top: size.height * 0.54,
+                  child: Text(
+                    'ポイント',
+                    style: TextStyle(
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 27,
+                      color: bottomColor,
+                    ),
+                  )
+              ),
+              Positioned(
+                  left: size.width * 0.47,
+                  top: size.height * 0.54,
+                  child: Text(
+                    point.toString() +' points',
+                    style: TextStyle(
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 27,
+                      color: bottomColor,
+                    ),
+                  )
+              ),
 
-                ]
-            )
-        )
+              // チップの配置
+              Positioned(
+                  left: size.width * 0.1,
+                  top: size.height * 0.60,
+                  child: Text(
+                    'チップ',
+                    style: TextStyle(
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 27,
+                      color: bottomColor,
+                    ),
+                  )
+              ),
+              Positioned(
+                  left: size.width * 0.47,
+                  top: size.height * 0.60,
+                  child: Text(
+                    chip.toString(),
+                    style: TextStyle(
+                      //fontWeight: FontWeight.bold,
+                      fontSize: 27,
+                      color: bottomColor,
+                    ),
+                  )
+              ),
+
+            ]
+        ),
 
     );
   }
