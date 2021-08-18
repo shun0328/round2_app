b81478511dedb23b6c23fec735def055bcc17b43
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-08-18 13:28:09 +0900

    ✨データベース接続

--- lib/ringgame_model.dart
+++ lib/ringgame_model.dart
@@ -1,17 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class RingGameModel extends ChangeNotifier {
+  final _ringGameCollection = FirebaseFirestore.instance.collection('ringGames').orderBy('tableName');
 
-  String tableName = 'A';
-  final time = '19:30';
-  final numMember = 6;
+  // リングゲームオブジェクトのリスト
+  List<RingGame>? ringGames;
 
-  final tableName02 = 'B';
-  final time02 = '17:00';
-  final numMember02 = 8;
+  // FireBaseからリングゲーム情報を取得
+  void fetchRingGame() async {
 
-  // メソッド
-  void sampleMethod() {
+    // データベースからデータを受け取る
+    final snapshot = await _ringGameCollection.get();
+    
+
+
+    // 受け取った情報からインスタンスを生成
+    final List<RingGame> ringGames = snapshot.docs.map((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+      final String time = data['time'];
+      final String ringGameName = data['tableName'];
+      final int numMember = data['numMember'];
+      return RingGame(ringGameName, numMember, time);
+    }).toList();
+
+    // トーナメントオブジェクトのリストが完成
+    this.ringGames = ringGames;
+
+    // 終わった事をviewに知らせる
     notifyListeners();
   }
+}
+
+
+// Ring Game object
+class RingGame{
+  // constructor
+  RingGame(this.tableName, this.numMember, this.time);
+  String tableName;
+  int numMember;
+  String time;
 }
--- lib/ringgame_view.dart
+++ lib/ringgame_view.dart
@@ -10,7 +10,7 @@ class RingGameView extends StatelessWidget {
 
     // providerパターンでModelを使用
     return ChangeNotifierProvider<RingGameModel>(
-      create: (_) => RingGameModel(),
+      create: (_) => RingGameModel()..fetchRingGame(),
       child: Scaffold(
         // 画面の背景色を設定
         backgroundColor: Colors.blueGrey,
@@ -27,82 +27,69 @@ class RingGameView extends StatelessWidget {
 
         // bodyの設定
         body: Consumer<RingGameModel>(builder: (context, model, child) {
-          return SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-
-
-                // リングゲームA
-                Column(
-                  children: <Widget>[
-                    Container(
-                      width: size.width,
-                      color: Colors.white,
-                      margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                      padding: EdgeInsets.fromLTRB(40, 10, 0, 14),
-                      child: Text('Table：' + model.tableName),
-                    ),
-                    Container(
-                      width: size.width,
-                      color: Colors.white,
-                      margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                      padding: EdgeInsets.fromLTRB(40, 0, 0, 10),
-                      child: Row(
-                        children: [
-                          Icon(Icons.account_circle, size: 30.0),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Text(model.numMember.toString() + ' / 9名'),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Text(model.time + ' 開始'),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-
-
-                // リングゲームB
-                Column(
-                  children: <Widget>[
-                    Container(
-                      width: size.width,
-                      color: Colors.white,
-                      margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                      padding: EdgeInsets.fromLTRB(40, 10, 0, 14),
-                      child: Text('Table：' + model.tableName02),
-                    ),
-                    Container(
-                      width: size.width,
-                      color: Colors.white,
-                      margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                      padding: EdgeInsets.fromLTRB(40, 0, 0, 10),
-                      child: Row(
-                        children: [
-                          Icon(Icons.account_circle, size: 30.0),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Text(model.numMember02.toString() + ' / 9名'),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Text(model.time02 + ' 開始'),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-
-
-              ],
-            ),
-          );
+          return viewRingGame(size, model.ringGames);
         }),
       ),
     );
+  }
+
+
+  // リングゲームViewを表示するためのカスタムWidgetを作成
+  Widget viewRingGame (Size size, List<RingGame>? ringGames) {
+
+    // 最後にWidgetのリスト"list"をColumnに包んでViewに渡す
+    List <Widget> list = [];
+
+    // ローディング画面
+    if(ringGames == null){
+      return Center(
+        child: Text('Loading...',
+          textAlign: TextAlign.center,),
+      );
+    }
+
+    // RingGameオブジェクトを元にレイアウトを行う
+    for(var i = 0; i < ringGames.length; i++) {
+      list.add(
+          Column(
+            children: <Widget>[
+              // リングゲームA
+              Column(
+                children: <Widget>[
+                  Container(
+                    width: size.width,
+                    color: Colors.white,
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    padding: EdgeInsets.fromLTRB(40, 10, 0, 14),
+                    child: Text('Table：' + ringGames[i].tableName),
+                  ),
+                  Container(
+                    width: size.width,
+                    color: Colors.white,
+                    margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                    padding: EdgeInsets.fromLTRB(40, 0, 0, 10),
+                    child: Row(
+                      children: [
+                        Icon(Icons.account_circle, size: 30.0),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(ringGames[i].numMember.toString() + ' / 9名'),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(ringGames[i].time + ' 開始'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+
+            ],
+          )
+      );
+    }
+    return SingleChildScrollView(child: Column(children: list));
   }
 }
