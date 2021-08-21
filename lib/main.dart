f6ff4c9b85dd023bdcb22d1b2b789137afa2acc4
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-08-21 20:44:15 +0900

    ♻️ リファクタリング

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/profile_view.dart';
 import 'package:flutter_app/setting_view.dart';
-import 'package:flutter_app/tournament_view.dart';
 
 import 'event_view.dart';
 import 'login_view.dart';
@@ -11,30 +10,24 @@ import 'time_line_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp();
-
-
-  // 最初に表示するWidget
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   static const String _title = 'Round2';
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
 
-      //home: HomePage(),
-      //Login画面を表示
-      home:  (() {
+      // ログイン済だとホームページにそうでなければログイン画面に遷移
+      home: (() {
         if (FirebaseAuth.instance.currentUser == null) {
           return LoginView();
         } else {
-          print(FirebaseAuth.instance.currentUser!.email!+"でログイン");
+          print(FirebaseAuth.instance.currentUser!.email! + "でログイン");
           return HomePage();
         }
       })(),
@@ -66,35 +59,26 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: Colors.lightBlueAccent,
-
       body: _pageList[_selectedIndex],
-
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-
           BottomNavigationBarItem(
             icon: Icon(Icons.home, size: 35.0),
             label: 'イベント',
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.account_circle_rounded, size: 35.0),
             label: 'プロフィール',
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.access_time, size: 35.0),
             label: 'タイムライン',
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.settings, size: 35.0),
             label: '設定',
           ),
-
         ],
-
         currentIndex: _selectedIndex,
         fixedColor: Colors.blueAccent,
         onTap: _onItemTapped,
@@ -102,10 +86,7 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-
 }
-
-
 
 // ログイン画面用Widget
 class LoginPage extends StatefulWidget {
@@ -120,15 +101,10 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-
         child: Container(
           padding: EdgeInsets.all(24),
           child: Column(
