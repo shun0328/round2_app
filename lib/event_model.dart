a142bdb50bc89a5ee1c5ce1b6e1e3943d507e339
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-08-21 19:33:35 +0900

    ♻️ リファクタリング

--- lib/event_model.dart
+++ lib/event_model.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class EventModel extends ChangeNotifier {
-  // メンバ変数
-  String sample = "sample";
-
+  // Instagramに遷移するための関数
   launchInstagram() async {
     const url = "https://www.instagram.com/";
     if (await canLaunch(url)) {
@@ -14,6 +12,7 @@ class EventModel extends ChangeNotifier {
     }
   }
 
+  // Lineに遷移するための関数
   launchLine() async {
     const url = "https://line.me/";
     if (await canLaunch(url)) {
@@ -23,6 +22,7 @@ class EventModel extends ChangeNotifier {
     }
   }
 
+  // Twitterに遷移するための関数
   launchTwitter() async {
     const url = "https://twitter.com/";
     if (await canLaunch(url)) {
@@ -32,6 +32,7 @@ class EventModel extends ChangeNotifier {
     }
   }
 
+  // Facebookに遷移するための関数
   launchFacebook() async {
     const url = "https://www.facebook.com/";
     if (await canLaunch(url)) {
@@ -39,10 +40,5 @@ class EventModel extends ChangeNotifier {
     } else {
       throw 'Could not Launch $url';
     }
-  }
-
-  // メソッド
-  void sampleMethod() {
-    notifyListeners();
   }
 }
