0e86d5441dbdfd7bdca88bf82e7a9baff5be6d46
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-08-21 20:40:53 +0900

    ♻️ リファクタリング

--- lib/login_model.dart
+++ lib/login_model.dart
@@ -2,14 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginModel extends ChangeNotifier {
-
   // 入力したメールアドレス・パスワード
   String email = '';
   String password = '';
 
-
   // Authenticationでログインを行う関数
-  Future login() async{
+  Future login() async {
     // メール・パスワードでログイン
     final FirebaseAuth auth = FirebaseAuth.instance;
     await auth.signInWithEmailAndPassword(
