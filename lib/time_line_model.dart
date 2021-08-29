e534f930c78cbbef4f2c718fd83c20659ef8ba4f
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-08-29 23:27:41 +0900

    🔖 データベースの情報を表示

--- lib/time_line_model.dart
+++ lib/time_line_model.dart
@@ -2,23 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class TimeLineModel extends ChangeNotifier {
-  //final Stream<QuerySnapshot> _tweetsSream =
-  //FirebaseFirestore.instance.collection('users').snapshots();
   final _tweetCollection = FirebaseFirestore.instance.collection('tweets');
 
   // トーナメントオブジェクトのリスト
   List<Tweet>? tweets;
-
-  void fetchTweet2(tweets) async {
-    for (int i = 0; i < tweets.length; i++) {
-      DocumentSnapshot snapshot = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(tweets[0].userId)
-          .get();
-      tweets[i].userName = await snapshot['nickName'];
-      print(snapshot['nickName']);
-    }
-  }
 
   // FireBaseからトーナメント情報を取得
   void fetchTweet() async {
@@ -29,13 +16,11 @@ class TimeLineModel extends ChangeNotifier {
       Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
       final String imageURL = data["imageURL"];
       final String text = data['text'];
-      final String userId = data['userId'];
+      //final String userId = data['userId'];
       final String userName = data['userName'];
       final String userImage = data['userImage'];
 
-      print(data['user']);
-
-      return Tweet(imageURL, text, userId, userName, userImage);
+      return Tweet(imageURL, text, userName, userImage);
     }).toList();
     // ツイートオブジェクトのリストが完成
     this.tweets = tweets;
@@ -43,34 +28,14 @@ class TimeLineModel extends ChangeNotifier {
     // 終わった事をviewに知らせる
     notifyListeners();
   }
-
-  // ドキュメントIDからユーザーの名前を取得する関数
-  String getName(userId) {
-    /*
-    DocumentSnapshot snapshot =
-        await FirebaseFirestore.instance.collection('users').doc(userId).get();
-    print(snapshot['nickName']);
-
-     */
-
-    notifyListeners();
-
-    return 'a';
-  }
-
-  // ドキュメントIDからユーザーの画像を取得する関数
-  String getImage(userId) {
-    return 'https://firebasestorage.googleapis.com/v0/b/round2-fb.appspot.com/o/profile.png?alt=media&token=29684e3b-9544-44b1-948f-2d2d0349f900';
-  }
 }
 
 // tweet object
 class Tweet {
   // constructor
-  Tweet(this.imageURL, this.text, this.userId, this.userName, this.userImage);
+  Tweet(this.imageURL, this.text, this.userName, this.userImage);
   String imageURL;
   String text;
-  String userId;
   String userName;
   String userImage;
 }
--- lib/time_line_view.dart
+++ lib/time_line_view.dart
@@ -26,71 +26,114 @@ class TimeLineView extends StatelessWidget {
         ),
 
         // bodyの設定
-        body: Consumer<TimeLineModel>(
-          builder: (context, model, child) {
-            // ローディング画面
-            if (model.tweets == null) {
-              return Center(
-                child: CircularProgressIndicator(
-                  valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
-                ),
-              );
-            } else {
-              return SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    // 投稿01
-                    Column(
-                      children: <Widget>[
-                        Container(
-                          width: size.width,
-                          color: Colors.white,
-                          margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                          padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
-                          child: Row(
-                            children: [
-                              Container(
-                                margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
-                                width: 50.0,
-                                height: 50.0,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  image: DecorationImage(
-                                    fit: BoxFit.fill,
-                                    image: NetworkImage(
-                                        model.tweets![0].userImage),
+        body: Stack(
+          children: [
+            Positioned(
+              //left: 0,
+              //top: 0,
+              //height: size.height,
+              child: Consumer<TimeLineModel>(
+                builder: (context, model, child) {
+                  // ローディング画面
+                  if (model.tweets == null) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        valueColor:
+                            new AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    );
+                  }
+                  // データが読み込めたらWidgetを作成
+                  else {
+                    // 最後にWidgetのリスト"list"を使って画面を構成する
+                    List<Widget> list = [];
+
+                    // RingGameオブジェクトを元にレイアウトを行う
+                    for (var i = 0; i < model.tweets!.length; i++) {
+                      list.add(
+                        Column(
+                          children: <Widget>[
+                            // 投稿01
+                            Column(
+                              children: <Widget>[
+                                Container(
+                                  width: size.width,
+                                  color: Colors.white,
+                                  margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                                  padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        margin:
+                                            EdgeInsets.fromLTRB(0, 0, 15, 0),
+                                        width: 50.0,
+                                        height: 50.0,
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          image: DecorationImage(
+                                            fit: BoxFit.fill,
+                                            image: NetworkImage(
+                                                model.tweets![i].userImage),
+                                          ),
+                                        ),
+                                      ),
+                                      Text(model.tweets![i].userName),
+                                    ],
                                   ),
                                 ),
-                              ),
-                              Text(model.tweets![0].userName),
-                            ],
-                          ),
+
+                                // 画像があれば画像を表示
+                                (() {
+                                  if (model.tweets![i].imageURL == '') {
+                                    return SizedBox();
+                                  } else {
+                                    return Container(
+                                      color: Colors.white,
+                                      margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                      padding:
+                                          EdgeInsets.fromLTRB(20, 10, 20, 10),
+                                      child: Image(
+                                          image: NetworkImage(
+                                              model.tweets![i].imageURL)),
+                                      width: double.infinity,
+                                    );
+                                  }
+                                })(),
+
+                                Container(
+                                  color: Colors.white,
+                                  margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                                  child: Text(
+                                    model.tweets![i].text,
+                                    style: TextStyle(fontSize: 15),
+                                    overflow: TextOverflow.visible,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                        Container(
-                          color: Colors.white,
-                          margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                          child: Image(
-                              image: NetworkImage(model.tweets![0].imageURL)),
-                          width: double.infinity,
-                        ),
-                        Container(
-                          color: Colors.white,
-                          margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                          child: Text(
-                            model.tweets![0].text,
-                            style: TextStyle(fontSize: 15),
-                            overflow: TextOverflow.visible,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              );
-            }
-          },
+                      );
+                    }
+
+                    // 作成したウィジェットのリストででSingleChildScrollViewを構成
+                    return SingleChildScrollView(child: Column(children: list));
+                  }
+                },
+              ),
+            ),
+            Positioned(
+              top: size.height * 0.68,
+              left: size.width * 0.78,
+              child: IconButton(
+                iconSize: size.height * 0.07,
+                icon: const Icon(Icons.add_circle),
+                color: Colors.lightBlue,
+                onPressed: () {},
+              ),
+            ),
+          ],
         ),
       ),
     );
