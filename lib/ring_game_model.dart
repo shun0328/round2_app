89f8c44e184b3cc5145bd785a9211b2c6d658123
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-09-03 00:51:47 +0900

    📚 データベースの情報を記述

--- lib/ring_game_model.dart
+++ lib/ring_game_model.dart
@@ -1,20 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class RingGameModel extends ChangeNotifier {
-  final _ringGameCollection = FirebaseFirestore.instance.collection('ringGames').orderBy('tableName');
+/*
+テーブル[ringGames]
+number  numMember
+string  tableName
+string  time
+*/
 
+class RingGameModel extends ChangeNotifier {
   // リングゲームオブジェクトのリスト
   List<RingGame>? ringGames;
 
-  // FireBaseからリングゲーム情報を取得
+  // FireBaseからリングゲーム情報を取得する関数
   void fetchRingGame() async {
-
     // データベースからデータを受け取る
+    final _ringGameCollection =
+        FirebaseFirestore.instance.collection('ringGames').orderBy('tableName');
     final snapshot = await _ringGameCollection.get();
 
     // 受け取った情報からインスタンスを生成
-    final List<RingGame> ringGames = snapshot.docs.map((DocumentSnapshot document) {
+    final List<RingGame> ringGames =
+        snapshot.docs.map((DocumentSnapshot document) {
       Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
       final String time = data['time'];
       final String ringGameName = data['tableName'];
@@ -30,9 +37,8 @@ class RingGameModel extends ChangeNotifier {
   }
 }
 
-
 // Ring Game object
-class RingGame{
+class RingGame {
   // constructor
   RingGame(this.tableName, this.numMember, this.time);
   String tableName;
