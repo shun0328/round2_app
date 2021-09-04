a9572a0b32a8161aef4cc3986c2794b2cd580c9a
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-09-04 16:54:25 +0900

    ♻️ コメントアウトの追加

--- lib/edit_name_model.dart
+++ lib/edit_name_model.dart
@@ -3,33 +3,34 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class EditNameModel extends ChangeNotifier {
-  // usersテーブルの中からログイン中の
-  // メールアドレスと一致するuserを抽出
-  final _userCollection = FirebaseFirestore.instance.collection('users').where(
-      "email",
-      isEqualTo: FirebaseAuth.instance.currentUser?.email ?? '');
-
   // ドキュメントID
-  String documentId = '';
-
+  String? documentId;
+  // 入力された名前
   String? newName;
 
-  // データベースからプロフィール情報を受け取る関数
-  void fetchProfile() async {
-    // データベースからデータを受け取る
+  // データベースからドキュメントIDを受け取る関数
+  void getDocId() async {
+    // usersテーブルの中からログイン中の
+    // メールアドレスと一致するuserを抽出
+    final _userCollection = FirebaseFirestore.instance
+        .collection('users')
+        .where("email",
+            isEqualTo: FirebaseAuth.instance.currentUser?.email ?? '');
+    // データベースからdocumentIDを受け取る
     final snapshot = await _userCollection.get();
-    // 受け取った情報からインスタンスを生成
-    documentId = snapshot.docs[0].id;
+    this.documentId = snapshot.docs[0].id;
     // 終わった事をviewに知らせる
     notifyListeners();
   }
 
+  // データベースの名前を更新する関数
   Future upDateName() async {
+    // 不備があったらエラーを返す
     if (newName == null) {
       throw ('名前が入力されていません');
     }
-    final _userCollection =
-        FirebaseFirestore.instance.collection('users').doc(documentId).update({
+    // データベースの名前を更新
+    FirebaseFirestore.instance.collection('users').doc(documentId).update({
       'nickName': newName,
     });
     notifyListeners();
